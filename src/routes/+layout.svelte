1b8176b3cc676efe64ed4cd4f1468a46a0b21d9d
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2023-06-30 18:15:10 +0400

    fix base for links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script>
+	import { base } from '$app/paths';
 	import '../app.css';
 </script>
 
 <div class="m-6">
 	<div class="navbar justify-center">
-		<a class="btn btn-ghost normal-case" href="/">order-book</a>
-		<a class="btn btn-ghost normal-case" href="/custom-candlestick">custom-candlestick</a>
-		<a class="btn btn-ghost normal-case" href="/tradingview-candlestick">tradingview-candlestick</a>
+		<a class="btn btn-ghost normal-case" href="{base}/">order-book</a>
+		<a class="btn btn-ghost normal-case" href="{base}/custom-candlestick">custom-candlestick</a>
+		<a class="btn btn-ghost normal-case" href="{base}/tradingview-candlestick"
+			>tradingview-candlestick</a
+		>
 	</div>
 
 	<slot />
